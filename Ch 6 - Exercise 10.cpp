be857af5da8534b9a92742a9a4f6e77fb1fad59b
Dillon <34174536+TingeOGinge@users.noreply.github.com>
2018-03-26 17:31:54 +0100

    Update Ch 6 - Exercise 10.cpp

--- Ch 6 - Exercise 10.cpp
+++ Ch 6 - Exercise 10.cpp
@@ -2,14 +2,13 @@
 //Combinations C(a,b) = P(a,b) / b! 
 
 int factorial(int a){
-//Factorial: if a == 5, then 5! = 5*4*3*2*1
 	constexpr int INT_MAX = numeric_limits<int>::max();
 	
 	if (a < 0) error("Factorial of a negative is undefined");
 	if (a == 0) return 1;
 	else 
 		for (int x = a - 1; x > 0; x--){
-			if (INT_MAX - a < a*x) error("factorial(); has overflowed the int")
+			if (INT_MAX - a < a * x) error("factorial(); int overflow")
 			a *= x; 
 		}
 	return a;
@@ -17,45 +16,52 @@ int factorial(int a){
 
 int permutations(int a, int b){
 	if (a < b) error("Permutations(); Cannot have a < b");
-	int value = 0;
-	value = factorial(a) / factorial(a-b);
-	return value; 
+	return factorial(a) / factorial(a-b);
 }
 
 int combinations(int a, int b){
 	return permutations(a,b) / factorial(b);
 }
 
-void print_results(int a){
-	cout << "= " << a << endl;
+int main(){
+	try
+	{
+	    while (cin) {
+		int a = 0, b = 0, value = 0; 
+		cin >> a >> b;
+		if (!cin) error("Number expected in main();") 
+		char ch; 
+		cin >> ch; 
+		switch (ch) {
+		case 'c': 
+			value = combinations(a,b); 
+			cout << "C(" << a << "," << b << ") = " << value << endl;
+			break;
+		case 'p': 
+			value = permutations(a,b); 
+			cout << "P(" << a << "," << b << ") = " << value << endl; 
+			break;
+		default: 
+			error ("'c' or 'p' expected in main()");
+			break;
+		}
+	    }
+	}
+	catch (exception& e) {
+	    cerr << "error: " << e.what() << '\n'; 
+		keep_window_open();
+	}
+	catch (...) {
+	    cerr << "Oops: unknown exception!\n"; 
+		keep_window_open();
+	}
 }
 
-int main()
-try
-{
-    while (cin) {
-	int a = 0, b = 0, value = 0; 
-        cin >> a >> b;
-	if (!cin) error("Number expected in main()") 
-	char ch; 
-	cin >> ch; 
-	switch (ch) {
-	case 'c': 
-		value = combinations(a,b); 
-	case 'p': 
-		value = permutations(a,b); 
-	default: 
-		error ("'c' or 'p' expected in main()");
+/*
+
+We know factorial(a) works as we have isolated this previously 
+
+Things to try and combat this non existent exception of dividing by zero
+	isolate factorial(a-b) to see if this is the issue 
 	
-	print_results(value);	
-	}
-    }
-}
-catch (exception& e) {
-    cerr << "error: " << e.what() << '\n'; 
-	keep_window_open();
-}
-catch (...) {
-    cerr << "Oops: unknown exception!\n"; 
-	keep_window_open();
-}
+*/
